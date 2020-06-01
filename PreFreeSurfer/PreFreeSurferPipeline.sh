516dada9a84077dbadfd40cb6ec430c71e489960
Kathy Snider <sniderk@ohsu.edu>
2020-06-01 12:24:32 -0700

    Using brain mask that is already acpc-aligned.

--- PreFreeSurfer/PreFreeSurferPipeline.sh
+++ PreFreeSurfer/PreFreeSurferPipeline.sh
@@ -643,11 +643,13 @@ if [ -n "${T1BrainMask}" ] ; then
     # the T1w mask to make the T2w mask.
 
     # Copy the user-supplied mask to ${T1wFolder}/${T1wImage}_brain_mask.
+    # The brain mask we are using here is already acpc aligned - KS 20200601.
     imcp ${T1BrainMask} ${T1wFolder}/${T1wImage}_brain_mask
+    imcp ${T1BrainMask} ${T1wFolder}/${T1wImage}_acpc_brain_mask
 
     # The T1w head was ACPC aligned in the loop above. Use the resulting
     # acpc.mat to align the mask.
-    ${FSLDIR}/bin/applywarp --rel --interp=nn -i ${T1wFolder}/${T1wImage}_brain_mask -r ${T1wTemplateBrain} --premat=${T1wFolder}/xfms/acpc.mat -o ${T1wFolder}/${T1wImage}_acpc_brain_mask
+    #${FSLDIR}/bin/applywarp --rel --interp=nn -i ${T1wFolder}/${T1wImage}_brain_mask -r ${T1wTemplateBrain} --premat=${T1wFolder}/xfms/acpc.mat -o ${T1wFolder}/${T1wImage}_acpc_brain_mask
 
     # Use the ACPC aligned T1w brain mask to extract the T1w brain.
     ${FSLDIR}/bin/fslmaths ${T1wFolder}/${T1wImage}_acpc -mas ${T1wFolder}/${T1wImage}_acpc_brain_mask ${T1wFolder}/${T1wImage}_acpc_brain
